e0294b35b017926378c14f081d50b8496134ee7a
Biro Biro <thiagomarquessp@gmail.com>
2016-02-27 13:32:17 -0300

    Create the verify historic feature.

---
+++ app/src/test/functional_tests/features/specifications/verify_historic.feature
@@ -0,0 +1,8 @@
+@history
+
+Feature: Validate historic
+
+  Scenario: As a user, I want validate the historic wanted
+
+    Given I press the Histórico button
+    When I select the CEP wanted
---
+++ app/src/test/functional_tests/features/step_definitions/verify_historic.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+#!/usr/bin/env ruby
+
+Given(/^I press the Histórico button$/) do
+  step "I press the Buscar Cep button"
+  step "entering the zip code data"
+  hide_soft_keyboard
+  step "I press the buscar button"
+  sleep 01
+  step "I go back"
+  sleep 01
+  touch("android.widget.Button id:'btnHistorico'")
+end
+
+When(/^I select the CEP wanted$/) do
+  pending # express the regexp above with the code you wish you had
+end
